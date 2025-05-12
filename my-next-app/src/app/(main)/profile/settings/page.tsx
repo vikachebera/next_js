5da6aebb1a8624ae51737b1b72e7acaef8527cfc
Viktoriia Chebera <cheberavika@gmail.com>
2025-05-12 15:46:05 +0200

    task1 completed

--- my-next-app/src/app/(main)/profile/settings/page.tsx
+++ my-next-app/src/app/(main)/profile/settings/page.tsx
@@ -1,7 +1,25 @@
+'use client'
+import { useEffect, useState } from "react";
+
 export default function SettingsPage() {
+    const [email, setEmail] = useState('');
+
+    const login = process.env.USER_LOGIN;
+    const password = process.env.USER_PASSWORD;
+
+    console.log("Login (server):", login);
+    console.log("Password (server):", password);
+
+    useEffect(() => {
+        setEmail(process.env.NEXT_PUBLIC_USER_EMAIL || '');
+        console.log("Email (browser):", process.env.NEXT_PUBLIC_USER_EMAIL);
+    }, []);
+
+
     return (
         <div>
             <h1>Profile Settings</h1>
+            <p>User email: {email}</p>
             <p>Manage your profile settings here.</p>
         </div>
     );
